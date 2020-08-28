8500fac326f2b7cbc43122906e738b3ec1b3db29
Brian Kallay <brian.kallay@gmail.com>
2020-08-28 06:29:12 -0700

    initial version

---
+++ scripts/variables.py
@@ -0,0 +1,47 @@
+##################################################################################################
+###########################      Variable Practice       #########################################
+##################################################################################################
+
+
+###########################          Strings             #########################################
+#1) Create a variable named greeting and assign it a string with the value of Hello, remember to use quotes around the string
+greeting = "hello,"
+
+#2) Use the print function to display the greeting variable to the screen
+print(greeting)
+
+#3) Create another variable called name and assign it the value of your name
+
+
+#4) Use the print funciton to display both the greeting and the name, use the + operator to concatenate the two strings
+
+
+#5) Notice that in the output above the two strings are stuck together without a space in between, fix it by
+#   concatenating a space in between the two variables
+
+
+#6) Try using the multiplication operator on your greeting variable, see what happens when you print greeting * 3
+
+
+
+
+###########################          Integers           #########################################
+#7) Create a variable named x and assign it the value 7
+
+
+#8) Use the print function to display the value of x to the screen
+
+
+#9) Create a another variable named y and assign it the value 3
+
+
+#10) Create a variable named result and assign it the value of x / y
+
+
+#11) Use the print funciton to display the value of result
+
+
+#14) Re-assign the value of result to x % y  (% is the modulus operator)
+
+
+#15) Use the print funciton to display the value of result
